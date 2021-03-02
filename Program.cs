873f7ed79653cec9f2c304f9d08c8c82031bd6b1
Vikash Singh <38196118+svikash@users.noreply.github.com>
2021-03-02 12:47:50 +0530

    upload programs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        StreamReader streamReader = null;
+        try
+        {
+            streamReader = new StreamReader(@"F:\New folder (3)\Data.txt");
+            Console.WriteLine(streamReader.ReadToEnd());
+            Console.ReadLine();
+        }
+        catch(FileNotFoundException ex)
+        {
+            Console.WriteLine("Check the file Name", ex.FileName);
+            Console.ReadLine();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+        }
+        finally
+        {
+            if (streamReader != null)
+            {
+                streamReader.Close();
+            }
+            Console.WriteLine("final block");
+            
+        }
+            }
+        }
+
+
+
+
+
+
+
+ 
---
+++ customhandling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+public class CustomException
+{
+    public static void Main()
+    {
+        try
+        {
+            throw new UserAlreadyLoggedInException("Already login no duplicate session ");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+        }
+    }
+
+}
+public class UserAlreadyLoggedInException : Exception
+{
+    public UserAlreadyLoggedInException() : base()
+    {
+
+    }
+   public UserAlreadyLoggedInException(string message) : base(message)
+    {
+
+    } 
+
+    public UserAlreadyLoggedInException(string message,Exception innerException) : base(message, innerException)
+    {
+
+    }
+}
